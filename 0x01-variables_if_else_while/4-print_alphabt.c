0e5b7efad6f5c2cfe209e3e8623d58d523d93a56
cloudtechy <conyekelu@yahoo.com>
2023-02-20 02:57:05 -0800

    prints alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* Description: 'print alphabet from a - z'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i;
+
+/* your code goes there */
+for (i = 'a';
+i < ('z' + 1); i++)
+{
+if (i == 'e' || i == 'q')
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
