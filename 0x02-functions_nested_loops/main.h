d825dcb0056046abaceb6c61719f9883c22276f5
cloudtechy <conyekelu@yahoo.com>
2023-02-21 17:44:07 -0800

    that prints all natural numbers from n to 98, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef mainn
-0;276;0c#define mainn
+#define mainn
 
 int  _putchar(char);
 void print_alphabet(void);
@@ -11,6 +11,7 @@ int print_last_digit(int);
 int _abs(int);
 void jack_bauer(void);
 void times_table(void);
+void print_to_98(int n);
 int add(int, int);
 
 #endif
