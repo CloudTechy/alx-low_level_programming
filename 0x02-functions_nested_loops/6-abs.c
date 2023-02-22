ac54df6be4389b63c9b78c60358013f9cd97338c
cloudtechy <conyekelu@yahoo.com>
2023-02-21 17:19:48 -0800

    computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - Entry point
+* @r: An input character
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (r * -1);
+}
