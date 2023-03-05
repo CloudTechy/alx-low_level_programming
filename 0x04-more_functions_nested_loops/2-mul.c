094d36fabf6f19f7b5b6aaaf24be0095f4233f19
cloudtechy <conyekelu@yahoo.com>
2023-03-05 00:05:20 -0800

    multiplies two values together

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - entry point
+* @a: the first integer parameter
+* @b: the second integer parameter
+* Description: 'A function that multiplies two numbers'
+* Return: an integer
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
