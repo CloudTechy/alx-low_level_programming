9b7e81488e8707d96267ef4d035192222bcb15a6
cloudtechy <conyekelu@yahoo.com>
2023-03-05 00:16:19 -0800

    prints numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers -  An entry point
+* Description: 'prints the numbers, from 0 to 9, followed by a new line'
+*/
+void print_numbers(void)
+{
+char i = '0';
+
+for (; i <= '9'; i++)
+{
+putchar(i);
+}
+putchar("\n");
+}
