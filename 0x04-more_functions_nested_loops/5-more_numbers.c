a67f29bacad585654f42cc5578690f6fce9dcecc
cloudtechy <conyekelu@yahoo.com>
2023-03-05 01:10:35 -0800

    prints 10 times the numbers, from 0 to 14, followed by a new line.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,12 @@ for (i = 0; i <= 10; i++)
 {
 for (j = 0; j < 14; j++)
 {
-_putchar(j);
+if (i > 9)
+_putchar(i / 10 + '0');
+
+_putchar(i % 10 + '0');
 }
 _putchar('\n');
 }
-_putchar('\n');
+
 }
