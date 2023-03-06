92efe9934bbf7ff9e69f2dc1b66dab7f5fbedea1
cloudtechy <conyekelu@yahoo.com>
2023-03-05 19:45:21 -0800

    fizzbuzz FIXED

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 int i;
 
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 5 == 0 && i % 3 == 0)
 {
@@ -29,6 +29,6 @@ else
 printf("%d ", i);
 }
 }
-printf("\n");
+printf("Buzz\n");
 return (0);
 }
