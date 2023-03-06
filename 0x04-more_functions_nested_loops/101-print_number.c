7412a02669691c8a8dd0577d49032e227b80db25
cloudtechy <conyekelu@yahoo.com>
2023-03-06 03:28:22 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_number -  An entry point
+* @n: integer parameter
+* Description: 'prints the number
+*/
+void print_number(int n)
+{
+int i = 1000000000;
+
+for (; i >= 1; i /= 10)
+if (m / i != 0)
+{
+_putchar((m / i) % 10 + '0');
+}
+}
