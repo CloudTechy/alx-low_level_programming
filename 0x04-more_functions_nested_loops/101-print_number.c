d4f2803b0a3f37b55a666a957634bec5737346b9
cloudtechy <conyekelu@yahoo.com>
2023-03-06 03:48:03 -0800

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+void print_integer(int m);
 
 /**
 * print_number -  An entry point
@@ -7,11 +8,33 @@
 */
 void print_number(int n)
 {
+if (n == 0)
+{
+_putchar('0');
+}
+else if (n < 0)
+{
+_putchar('-');
+print_integer(n * -1);
+}
+else
+print_integer(n);
+}
+
+/**
+ * print_integer - A function to printing n
+ * @m: an input unsigned integer
+ * Return: Nothing
+ */
+void print_integer(int m)
+{
 int i = 1000000000;
 
 for (; i >= 1; i /= 10)
-if (n / i != 0)
 {
-_putchar((n / i) % 10 + '0');
+if (m / i != 0)
+{
+_putchar((m / i) % 10 + '0');
+}
 }
 }
