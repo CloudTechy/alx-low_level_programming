2d8b7c52321933f3a198aea915c43afdbbd86c34
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-10-23 05:39:39 -0700

    done

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry function
+ * @dest: string para
+ * @src: string para
+ * @n: integer
+ * Description: "copies memory area"
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+		dest[i] = src[i];
+	return (dest);
+}
