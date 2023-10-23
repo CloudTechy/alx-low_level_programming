91243bb6ac4fd41ba8083d3d41763b309911e684
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-10-23 06:17:44 -0700

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strchr - entry function
+ * @s: string
+ * @c: character to search
+ * Description: "locates a character in a string"
+ * Return: a char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
