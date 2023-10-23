a45a7cbe3d528046bb697357aff2fef0b7f8f361
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-10-23 05:21:52 -0700

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h";
+
+/**
+ * _memset - entry function
+ * @s: a string
+ * @b: a character byte
+ * @n: an unsigned integer
+ * Description: "fill memory with a constant byte"
+ * Return: a pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+		*(s + i) = b;
+	return (*(s));
+
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef mainn
+#define mainn
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
