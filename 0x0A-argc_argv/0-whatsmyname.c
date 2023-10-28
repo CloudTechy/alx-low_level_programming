992c838065a13a4c516b0d406dc7d400e1827856
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-10-27 19:34:02 -0700

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - entry function.
  * @argc: arguments counter
- * @argv: arguments 
+ * @argv: arguments
  * Descriptiom: "prints its name"
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	while(*argv[0])
+	while (*argv[0])
 	{
 		_putchar(*argv[0]);
 		argv[0]++;
