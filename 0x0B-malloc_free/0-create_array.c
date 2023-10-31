a7547446c5547b3dfbf103d5c1d9b4c467a11be7
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-10-31 13:21:10 -0700

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * create_array - entry function
+ * @size: size of array
+ * @c: initiating character
+ * Description: "function that creates an array of chars,
+ * and initializes it with a specific char"
+ * Return: a pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
