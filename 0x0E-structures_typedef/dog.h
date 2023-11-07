4c817c9200749ca91e7115e37049cf310d789465
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-11-06 19:25:02 -0800

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a basic dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Description: data structure for dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
