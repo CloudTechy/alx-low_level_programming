c96bc499a9c9d0ad295a35358e664c6a78c64d48
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-11-08 09:29:23 -0800

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - prints name
+ * @name: string
+ * @f: callback function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
