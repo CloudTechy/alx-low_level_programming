eac98792fd872cec39504c47e050a0491ed6ac24
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-11-09 13:08:09 -0800

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum of all its parameters.
+ * @n: int
+ * @...: variadic parameters
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+
+	va_list item;
+	int i, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(item, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(item, int);
+	va_end(item);
+	return (sum);
+}
