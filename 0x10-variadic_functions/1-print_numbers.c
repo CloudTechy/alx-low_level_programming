5aaca457b5e5234f9e7be556ebcd57906bf6b0f4
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2023-11-09 13:58:28 -0800

    done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,20 +8,16 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (separator != NULL)
-	{
-		va_list list;
-		unsigned int i;
+	va_list list;
+	unsigned int i;
 
-		va_start(list, n);
-		for (i = 0; i < n; i++)
-			printf("%d", va_arg(list, int));
-			if (separator && i < n - 1)
-				printf("%s", separator);
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		printf("%d", va_arg(list, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 
-
-		va_end(list);
-	}
+	va_end(list);
 	printf("\n");
 
 }
