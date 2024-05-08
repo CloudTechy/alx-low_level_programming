3f2fd76afccd6dbadad494d1e7c3d3553e4f4d5c
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-05-08 10:46:24 +0100

    done

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ *
+ * @array: is a pointer to the first element of the array to search in
+ * @size:  the number of elements in array
+ * @value: the value to search for
+ * Return: "value index of array found or -1 when not found"
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+int i;
+
+/*Check if the array is NULL */
+if (array == NULL)
+{
+return (-1); /* Return -1 if array is NULL */
+}
+
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[0] = [%d]\n", array[i]);
+if (array[i] == value)
+return (array[i]);
+}
+
+return (-1); /* Return -1 if value is not found */
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH
+#define __SEARCH
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+
+#endif
