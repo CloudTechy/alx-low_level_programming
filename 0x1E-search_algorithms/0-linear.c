c7c927f6e08a21b1e108ebae1bcde0aed4d1d4dc
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-05-08 11:02:21 +0100

    done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,9 +22,9 @@ return (-1); /* Return -1 if array is NULL */
 
 for (i = 0; i < size; i++)
 {
-printf("Value checked array[0] = [%d]\n", array[i]);
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
-return (array[i]);
+return (i);
 }
 
 return (-1); /* Return -1 if value is not found */
